acb212a4b4960590ab54d3a50cd4475f52cb2e91
mob <mob@mikeobrien.net>
2012-04-23 20:43:37 -0400

    Fixed a bug in the ajax date parser

--- jquery/jquery.ajax-date-parser.js
+++ jquery/jquery.ajax-date-parser.js
@@ -8,18 +8,21 @@
         $.ajaxPrefilter(function(ajax) {
             if (!ajax.success) return;
             var success = ajax.success;
-
-            var parseValue = function(name, value) {
+            var getType = function(obj) { return Object.prototype.toString.apply(obj) }
+            var objectType = getType({}), arrayType = getType([]);
+            
+            var parseValue = function(value) {
                 if (typeof value === 'string' && !value.search(/^\/Date\(\d+\)\/$/))
                     return new Date(parseInt(value.substr(6)));
                 else return value;
             };
 
              var parseObject = function(obj) {
+                if (getType(obj) != objectType && getType(obj) != arrayType) return;
                 for (key in obj) {
                     var value = obj[key];
-                    if (typeof value === 'object') parseObject(value);
-                    else if (typeof value != 'function') obj[key] = parseValue(key, value);
+                    if (getType(value) === objectType || getType(value) === arrayType) parseObject(value);
+                    else if (typeof value != 'function') obj[key] = parseValue(value);
                 }
             };
 
